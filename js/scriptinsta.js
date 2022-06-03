c664d278818161c0c2bf6d8eb692bb1c080102f1
Allan Augusto <allan_wsg@hotmail.com>
2022-06-03 08:54:52 -0300

    second commit

--- js/scriptinsta.js
+++ js/scriptinsta.js
@@ -1,71 +1,31 @@
-const carrouselSlider = {
-    el: {
-        instaContainer: $('#insta')
-    },
+$(function(){
 
-    handleBuildCarrousel(data) {
-        // destructure data object
-        const {
-            caption,
-            id,
-            media_type,
-            media_url,
-            permalink,
-        } = data
+    const token = "IGQVJVZA1ZAKUjlibUhDNW9nSWszY1M3SVlBS18yT2E4TzNUSDRLb0JyMUVGNHpJaGFOZAWtfYXBKX1ZApRVpRZAVExZAS11bzJOYjhZAYUswdUl3WjVrSEl0S1FFRmd0TEJkcDIwVDhvOVNEWFBZARGhCRTV4dwZDZD";
+    const url = "https://graph.instagram.com/me/media?access_token=" + token + "&fields=media_url,media_type,caption,permalink";
 
-        // build html string
-        const carrouselSliderContainer = `
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="1000">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    </ol>
-                    <div class="carousel-inner">                    
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-            `;
+    $.get(url).then(function(response){
 
-        this.el.instaContainer.append(carrouselSliderContainer);
+        console.log('retorno: ', response.data);
 
-        // loop through data and append to carrousel
-        data.map((feedItem, index) => {
-            const videoContent = `<div class="carousel-item ${index === 0 ? 'active':''}"><div class="col-6 col-md-4" controls><source src="${feedItem.media_url}" type="video/mp4></video></div></div>`;
-            const imageContent = `<div class="carousel-item ${index === 0 ? 'active':''}"><div class="col-6 col-md-4"><img src="${feedItem.media_url}" alt="${feedItem.caption}" class="img-fluid"></div></div>`;
-            const contentInsideCarouselItem = media_type === 'video' ? videoContent : imageContent;
+        let dadosJson = response.data
+        let conteudo = '<div class="row">';
 
-            $('.carousel-inner').append(contentInsideCarouselItem)
-        })
-    },
-
-    event() {
-        const token = "IGQVJVZA1ZAKUjlibUhDNW9nSWszY1M3SVlBS18yT2E4TzNUSDRLb0JyMUVGNHpJaGFOZAWtfYXBKX1ZApRVpRZAVExZAS11bzJOYjhZAYUswdUl3WjVrSEl0S1FFRmd0TEJkcDIwVDhvOVNEWFBZARGhCRTV4dwZDZD"
-        const url = `https://graph.instagram.com/me/media?access_token=${token}&fields=media_url,media_type,caption,permalink`
-
-        const carrouselEmpty = `<div class="col-6 col-md-4 mb-3">Galeria do Instagram vazia</div>`
-
-        $.get(url).then((response) => {
-            // check if response is empt
-            if (!response.data) {
-                return this.el.instaContainer.append(carrouselEmpty);
+        for (let p = 0; p < 6 ; p++) {
+            let feed = dadosJson[p];
+            let titulo = feed.caption !== null ? feed.caption : '';
+            let tipo = feed.media_type;
+            if(tipo === 'VIDEO'){ 
+                conteudo += '<div class="col-6 col-md-4" controls><source src="'+feed.media_url+'" type="video/mp4></video></div>';
+            }if(tipo === 'CAROUSEL_ALBUM'){
+                conteudo += '<div class="col-6 col-md-4 mb-3"><img src="'+feed.media_url+'" class="d-block w-100" alt="Second slide"></div>';
             }
+             else if (tipo === 'IMAGE') {
+                conteudo += ' <div class="col-6 col-md-4 mb-3" ><img src="'+feed.media_url+'" class="d-block w-100" alt="First slide"></div>';
+                }        
+        }
+        conteudo += '</div>';
+        $('#insta').html(conteudo);
+    })
 
-            // call function to build carrousel
-            this.handleBuildCarrousel(response.data);
-        })
-    },
-
-    init() {
-        this.event();
-    }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    carrouselSlider.init()
 })
+
