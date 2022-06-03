64ce09412e65e3a29b4c9604f1a16f9598136ac9
Allan Augusto <allan_wsg@hotmail.com>
2022-06-03 13:32:57 -0300

    Update scriptinsta.js

--- js/scriptinsta.js
+++ js/scriptinsta.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    const token = "{{secrets.INSTA_TOKEN_SECURITY}}";
+    const token = secrets.INSTA_TOKEN_SECURITY;
     const url = "https://graph.instagram.com/me/media?access_token=" + token + "&fields=media_url,media_type,caption,permalink";
 
     $.get(url).then(function(response){
