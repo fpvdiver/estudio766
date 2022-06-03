b2be7defdfacfedb237b0e2540533fda10b2bb74
Allan Augusto <allan_wsg@hotmail.com>
2022-06-03 08:45:58 -0300

    first commit

---
+++ js/config.js
@@ -0,0 +1,30 @@
+// Configuration Object
+var config = {};
+
+/*-------------------------------------------------------------------*/
+/*      1. Form Messages
+/*-------------------------------------------------------------------*/
+
+config.formMessages = {
+    sent          : 'Menssagem enviada com sucesso. Obrigado!',
+    fail          : 'Oops! erro no envio. tente novamente',
+    failEmail     : 'E-mail inválido!',
+    emptyFields   : 'Complete todas as opções'
+};
+
+/*-------------------------------------------------------------------*/
+/*      2. Google Map
+/*-------------------------------------------------------------------*/
+
+config.gmap = {
+    latitude    : '-23.610742862710843',
+    longitude   : '-46.63818468846453',
+    zoom        : 15,
+    marker      : 'img/marker.png'
+};
+
+/*-------------------------------------------------------------------*/
+/*      3. Easy Background System Settings
+/*-------------------------------------------------------------------*/
+
+config.backgrouns = {};
